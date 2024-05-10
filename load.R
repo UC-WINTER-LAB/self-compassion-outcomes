728a0be3c42f8224c8c027e6b7c9285c79cfee92
Haututu <taylor.winter@canterbury.ac.nz>
2024-05-10 20:07:35 +1200

    Created coalesced ID from each timepoint (NAs were duplicating rows on joining).

--- load.R
+++ load.R
@@ -7,20 +7,21 @@ raw_df <- drop_read_csv("/Winter Data/Longitudinal Colleges Data - Otago/Belongi
 
 # Convert the data from wide format to long format
 raw_df_long <- raw_df %>%
-  select(-ends_with("_s1"), -ends_with("_s2"), -ends_with("_s3")) %>%
+  mutate(id = coalesce(id, id_s1, id_s2, id_s3)) %>% # Take ID from a later wave if its not in base ID variable
+  select(id, -ends_with("_s1"), -ends_with("_s2"), -ends_with("_s3")) %>%
   # Join the base demographic data onto each waves variables
   left_join(
     # Stack each timepoint on top of each other and trim the suffix
     bind_rows(
       raw_df %>%
-        select(ends_with("_s1")) %>%
+        select(id, ends_with("_s1"), -id_s1) %>% # Remove each waves ID to use the general coalesced one
         rename_with(~str_remove(., "_s1")),
       raw_df %>%
-        select(ends_with("_s2")) %>%
+        select(id, ends_with("_s2"), -id_s2) %>%
         rename_with(~str_remove(., "_s2")) %>%
         select(-college), # This wave had a college variable in it for some reason
       raw_df %>%
-        select(ends_with("_s3")) %>%
+        select(id, ends_with("_s3"), -id_s3) %>%
         rename_with(~str_remove(., "_s3"))
     ),
     by="id" # Assuming each waves ID variable is complete and consistent
