6be9610e4fec858d1dcd0f2bd4f67c890c1658e4
poppief <pfr47@uclive.ac.nz>
2024-06-18 15:16:41 +1200

    yaaasssss

--- merge.R
+++ merge.R
@@ -1,10 +1,15 @@
 
-df_merged <- bind_cols(
-  df_test %>%
-    select(id, time, contains("bpns")) %>%
-    na.omit() %>%
-    select(id, time),
-  predict(fit.sdt.three)
+df_merged <- df_test %>%
+  select(id, time, age, gender) %>%
+  left_join(
+    bind_cols(
+      df_test %>%
+        select(id, time, contains("bpns")) %>%
+        na.omit() %>%
+        select(id, time),
+      predict(fit.sdt.three)
+    ),
+    by=c("id", "time")
   ) %>%
   left_join(
     bind_cols(
@@ -17,5 +22,5 @@ df_merged <- bind_cols(
     by=c("id", "time")
   )
   
-
+#### copy from left join : contains ("Wellbeing shiz)
 
